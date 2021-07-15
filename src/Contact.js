1d159a67470b54a85c1fc65e11b27a1e63fdc3f3
harsha-pixel-s <harsha91005@gmail.com>
2021-07-15 20:37:41 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+       
+             <nav className="navbar">
+            
+            <h2>Contact</h2>
+        <div className="Links">
+            <a href="/">Home</a>
+            <a href="/Contact.js">Contact</a>
+        </div>
+        </nav>
+         
+        
+      );
+}
+ 
+export default Contact;
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+const Home = () => {
+    return ( 
+        
+        <nav className="navbar">
+            
+            <h2>Homepage</h2>
+        <div className="Links">
+            <a href="/">Home</a>
+            <a href="/Contact.js">Contact</a>
+            
+        </div>
+
+        </nav>
+      
+     );
+}
+ 
+export default Home;
